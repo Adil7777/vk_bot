261f59e1758ab364f00e09da7ff73c9430fd74ab
Adil7777 <57364623+Adil7777@users.noreply.github.com>
2020-09-08 22:09:53 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import vk_api
 import time
+import random
 
 token = 'c5484a72441332239935574e64a05dde05d9ac201015566fca92cc865291f0280aa446d740fe2fc4fc214'
 vk = vk_api.VkApi(token=token)
@@ -10,7 +11,7 @@ while True:
         id = messages["items"][0]["last_message"]["from_id"]
         body = messages["items"][0]["last_message"]["text"]
         if body.lower() == 'привет':
-            vk.method('messages.send', {"peer_id": id, "message": "Привет!", "random_id": 123456})
+            vk.method('messages.send', {"peer_id": id, "message": "Привет!", "random_id": random.randint(1, 2147483647)})
         else:
-            vk.method('messages.send', {"peer_id": id, "message": 'Я тебя не понял', "random_id": 123456})
+            vk.method('messages.send', {"peer_id": id, "message": 'Я тебя не понял', "random_id": random.randint(1, 2147483647)})
     time.sleep(1)
